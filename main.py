5cba1b200731897eda940737c2708324ebbc667e
Pieter <pietdagamer@gmail.com>
2018-11-17 14:29:16 +0100

    Move start_program in class

--- main.py
+++ main.py
@@ -7,42 +7,13 @@ import json
 from pprint import pprint
 from functools import partial
 
-PATH = os.path.dirname(os.path.realpath(__file__)) + "/"
-
-def start_project(project, globalFields):
-    global PATH
-    commands = ""
-
-    for field, value in globalFields.items():
-        commands += field + "=" + value + ";\n"
-
-    if 'fields' in project:
-        for field, value in project['fields'].items():
-            commands += field + "=" + value + ";\n"
-
-    if 'terminals' in project:
-        for terminal, spec in project['terminals'].items():
-            rcpath = PATH + "rc"
-            # cd = "cd " + spec['location'] + "; "
-            commands += "gnome-terminal --working-directory " + spec['location'] + \
-                    " -e \"bash -c \\\" " + spec['command'] + \
-                    "; exec bash --rcfile \\\"" + rcpath + "\\\" \\\"\"\n"
-
-    if 'windows' in project:
-        for window, command in project['windows'].items():
-            commands += command + ";\n"
-
-    print(commands)
-
-    os.system(commands)
-    QApplication.instance().quit()
 
 class Starter(QWidget):
 
     def __init__(self):
         super().__init__()
 
-
+        self.PATH = os.path.dirname(os.path.realpath(__file__)) + "/"
 
         self.initUI()
         self.searchText = ""
@@ -53,25 +24,24 @@ class Starter(QWidget):
 
         counter = 0
         self.buttons = []
-        self.project = {}
 
         error = None
-        with open(PATH + 'projects.json') as f:
+        with open(self.PATH + 'projects.json') as f:
             try:
-                self.projects = json.load(f)
+                self.config = json.load(f)
             except Exception as e:
                 error = e
 
         if error == None:
-            windowHeight = len(self.projects['projects']) * 30 + 30
-            self.projects['projects'] = \
-                    sorted(self.projects['projects'], key=lambda project: project['name'])
-            pprint(self.projects)
+            windowHeight = len(self.config['projects']) * 30 + 30
+            self.config['projects'] = \
+                    sorted(self.config['projects'], key=lambda project: project['name'])
+            pprint(self.config)
 
-            for project in self.projects['projects']:
-                if project["enabled"]:
+            for project in self.config['projects']:
+                if "enabled" in project and project["enabled"]:
                     btn = QPushButton(project['name'], self)
-                    btn.clicked.connect(partial(start_project, project))
+                    btn.clicked.connect(partial(self.start_project, project))
                     btn.resize(300, 30)
                     btn.move(0, (counter+1)*30)
                     btn.setStyleSheet("background-color: " + project['color'])
@@ -110,13 +80,41 @@ class Starter(QWidget):
 
     def keyPressEvent(self, event):
         if event.key() == QtCore.Qt.Key_Return:
-            filtered = [project for project in self.projects['projects']
+            filtered = [project for project in self.config['projects']
                     if self.searchText in project['name'].lower()]
 
             if len(filtered) > 0:
-                start_project(filtered[0], self.projects['globalfields'])
+                self.start_project(filtered[0])
         elif event.key() == QtCore.Qt.Key_Escape:
             QApplication.instance().quit()
+
+    def start_project(self, project):
+        commands = ""
+        globalFields = self.config['globalfields']
+
+        for field, value in globalFields.items():
+            commands += field + "=" + value + ";\n"
+
+        if 'fields' in project:
+            for field, value in project['fields'].items():
+                commands += field + "=" + value + ";\n"
+
+        if 'terminals' in project:
+            for terminal, spec in project['terminals'].items():
+                rcpath = self.PATH + "rc"
+                # cd = "cd " + spec['location'] + "; "
+                commands += "gnome-terminal --working-directory " + spec['location'] + \
+                        " -e \"bash -c \\\" " + spec['command'] + \
+                        "; exec bash --rcfile \\\"" + rcpath + "\\\" \\\"\"\n"
+
+        if 'windows' in project:
+            for window, command in project['windows'].items():
+                commands += command + ";\n"
+
+        print(commands)
+
+        os.system(commands)
+        QApplication.instance().quit()
 
 
 
